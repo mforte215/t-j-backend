de9f86adcf7cb36c6956d1329c2895d52b70366b
mforte215 <mark.fortee@gmail.com>
2024-02-20 09:42:05 -0500

    updated server settings

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const {ApolloServer} = require('@apollo/server');
 const {expressMiddleware} = require('@apollo/server/express4');
+const path = require('path');
 const {authMiddleware} = require('./utils/auth');
 
 const {typeDefs, resolvers} = require('./schemas');
@@ -23,6 +24,15 @@ const startApolloServer = async () => {
     app.use('/graphql', expressMiddleware(server, {
         context: authMiddleware
     }));
+
+
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.join(__dirname, '../client/dist')));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+        });
+    }
 
 
     db.once('open', () => {
